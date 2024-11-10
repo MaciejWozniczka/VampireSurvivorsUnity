94bf6ef1ad2d4e0b0e81c2619f681a12388ebfb8
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2024-11-10 13:52:38 +0100

    Added changing sides of enemy accordint to position of player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -13,5 +13,14 @@ public class EnemyController : MonoBehaviour
     void Update()
     {
         theRigibody.velocity = (target.position - transform.position).normalized * moveSpeed;
+
+        if (target.position.x < transform.position.x)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        else if (target.position.x > transform.position.x)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
     }
 }
